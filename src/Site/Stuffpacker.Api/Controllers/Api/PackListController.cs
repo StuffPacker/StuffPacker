af93ac31c4be9adaa7163ae3477bc51d46a1c340
Daniel Berg <daniel.berg@l58.se>
2020-01-17 08:57:44 +0100

    fixed login auth customer id

--- src/Site/Stuffpacker.Api/Controllers/Api/PackListController.cs
+++ src/Site/Stuffpacker.Api/Controllers/Api/PackListController.cs
@@ -24,7 +24,7 @@ namespace Stuffpacker.Api.Controllers.Api
         {
             var tempid = Guid.Parse("4f22d3fc-80fc-44b6-9e4b-f760d8b57876");
 
-           var list= await _packListService.Get(tempid);
+           var list= await _packListService.Get(GetUserId());
 
             return this.Ok(list);
         }
