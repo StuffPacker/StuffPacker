2a00ae63ac5c5d77ff0ae5f45d8e93710fe058c2
Daniel Berg <daniel.berg@l58.se>
2020-01-17 15:11:01 +0100

    minimal startup

--- src/Site/StuffPacker.Api.ApiHost/Startup.cs
+++ src/Site/StuffPacker.Api.ApiHost/Startup.cs
@@ -38,27 +38,27 @@ namespace StuffPacker.Api.ApiHost
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            ConfigureOptions(services, Configuration);
+            //ConfigureOptions(services, Configuration);
             //services.AddDbContext<StuffPackerDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddStuffPackerApiHostServices(Configuration, LoggerFactory);
-            var key = Encoding.ASCII.GetBytes(SiteOptions.ApiSecret);
-            services.AddAuthentication(x =>
-            {
-                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-            .AddJwtBearer("ApiBearer", x =>
-            {
-                x.RequireHttpsMetadata = false;
-                x.SaveToken = true;
-                x.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(key),
-                    ValidateIssuer = false,
-                    ValidateAudience = false
-                };
-            });
+            //services.AddStuffPackerApiHostServices(Configuration, LoggerFactory);
+            //var key = Encoding.ASCII.GetBytes(SiteOptions.ApiSecret);
+            //services.AddAuthentication(x =>
+            //{
+            //    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            //    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            //})
+            //.AddJwtBearer("ApiBearer", x =>
+            //{
+            //    x.RequireHttpsMetadata = false;
+            //    x.SaveToken = true;
+            //    x.TokenValidationParameters = new TokenValidationParameters
+            //    {
+            //        ValidateIssuerSigningKey = true,
+            //        IssuerSigningKey = new SymmetricSecurityKey(key),
+            //        ValidateIssuer = false,
+            //        ValidateAudience = false
+            //    };
+            //});
             services.AddRazorPages();
         }
 
@@ -72,8 +72,8 @@ namespace StuffPacker.Api.ApiHost
             app.UseHttpsRedirection();
            
             app.UseRouting();
-            app.UseAuthentication();
-            app.UseAuthorization();
+            //app.UseAuthentication();
+            //app.UseAuthorization();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
