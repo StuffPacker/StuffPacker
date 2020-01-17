abc77ce102710bb2fa2f5b2a6ea8f05b4be89e31
Daniel Berg <daniel.berg@l58.se>
2020-01-17 20:18:08 +0100

    more functions

--- src/Site/StuffPacker.Api.ApiHost/Controllers/PackListController.cs
+++ src/Site/StuffPacker.Api.ApiHost/Controllers/PackListController.cs
@@ -7,19 +7,17 @@ namespace StuffPacker.Api.ApiHost.Controllers
     [Route("api/v1/packlist")]
     public class PackListController : BaseController
     {
-        // private readonly IPackListService _packListService;
+        private readonly IPackListService _packListService;
 
-        //public PackListController(IPackListService packListService)
-        //{
-        //    _packListService = packListService;
-        //}
+        public PackListController(IPackListService packListService)
+        {
+            _packListService = packListService;
+        }
 
         [HttpGet("")]
         public async Task<IActionResult> Get()
         {
-            // var tempid = Guid.Parse("4f22d3fc-80fc-44b6-9e4b-f760d8b57876");
-
-            //var list= await _packListService.Get(GetUserId());
+            var list= await _packListService.Get(GetUserId());
 
             return this.Ok("ok");
         }
